48936d595e83671f4de1cf95de155b94455f8348
✨ <e@universe.local>
2022-09-02 09:32:32 -0400

    Fixing issues with NEAT.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# FedNEAT
+# FedNeuro
 
 **How it works:**
 
@@ -8,65 +8,28 @@
 
 3. run for up to 300 generations on the client 
 
-4. save results to Python pickle on the client
+4. save results to bitstring representation of genome
 
-5. send pickle to server
+5. send bitstring (genome) to server
 
-6. restore progress from each pickle - take one model and have it learn from avery other model
+6. restore progress from each genome - take one genome and make it evolve using knowledge from every other genome  (Bayes)
 
 
 ## Why?
 
-
 "Federated learning enables multiple actors to build a common, robust machine learning model without sharing data" - Wikipedia
 
-FedNEAT doesn't require clients to send data to the server. FedNEAT builds a "common" model by learning from the progress each client made training populations on their own device.
+FedNeuro doesn't require clients to send data to the server. FedNeuro builds a "common" model by learning from the progress each client made training populations on their own device.
 
-NEAT (neuroevolution in general) is less computationally expensive than gradient based learning. Computational efficiency is the initial reason I first started looking into this.
+## To-do
 
-## Code notes
+The dataset should 
+1. yield the model 
+2. narrow the search space
+
+CRDTs could help with 
+1. encoding
+2. genome deduplication
+3. novelty search
 
 
-The main function loads the configuration file, which is static and typed out by hand.
-```python
-
-# this is the main function
-def run(config_file):
-    # Load configuration.
-  config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                         config_file) # we only modified the activation functions really 
-                         # the rest are defaults
-
-```
-The population consists of individuals. Individuals are genomes. Each genome contains two sets of genes that describe how to build an artificial neural network.
-
-Here, we create a population:
-```python
-    # Create the population, which uses our configuration to create individual genomes.
-    p = neat.Population(config)
-```
-
-We have a fitness function. This functions like a loss function for gradient based learning - how do you decide which models perform well?
-```python
-#fitness function
-    def eval_genomes(genomes, config):
-    # each genome includes instructions for creating a neural network
-    # config = number of generations to run the algorithm for 
-```
-
-Here, we evaluate the fitness of a population: 
-```python
-    # this starts at generation 0
-    # run evolution algorithm run with the eval_genomes fitness function - for 10 generations
-    winner = p.run(eval_genomes, 10)
-    # winner returns the best genome seen
-```
-
-This is how you save the progress you've made (checkpoint that can be restored on server):
-
-```python
- #creates a checkpoint at the 10th generation!
-    p.add_reporter(neat.Checkpointer(11)) 
-```
-I am working on saving progress without sending it between the server and client in pickle form within `saver.py`.
--- client.py
+++ client.py
@@ -1,71 +1,49 @@
-"""
-2-input XOR example -- this is most likely the simplest possible example.
-"""
+# spectral bias code
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
 
-import os
+from argparse import Namespace
+from functools import reduce
+from copy import deepcopy
 
-import neat
+# spectral bias
+def make_phased_waves(opt):
+    t = np.arange(0, 1, 1./opt.N)
+    if opt.A is None:
+        yt = reduce(lambda a, b: a + b, 
+                    [np.sin(2 * np.pi * ki * t + 2 * np.pi * phi) for ki, phi in zip(opt.K, opt.PHI)])
+    else:
+        yt = reduce(lambda a, b: a + b, 
+                    [Ai * np.sin(2 * np.pi * ki * t + 2 * np.pi * phi) for ki, Ai, phi in zip(opt.K, opt.A, opt.PHI)])
+    return t, yt
 
-# dataset
-xor_inputs = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]
-xor_outputs = [(0.0,), (1.0,), (1.0,), (0.0,)]
+def fft(opt, yt):
+    n = len(yt) # length of the signal
+    k = np.arange(n)
+    T = n/opt.N
+    frq = k/T # two sides frequency range
+    frq = frq[range(n//2)] # one side frequency range
+    # -------------
+    FFTYT = np.fft.fft(yt)/n # fft computing and normalization
+    FFTYT = FFTYT[range(n//2)]
+    fftyt = abs(FFTYT)
+    return frq, fftyt
 
-#fitness function
-def eval_genomes(genomes, config):
-    for genome_id, genome in genomes:
-        genome.fitness = 4.0
-        net = neat.nn.FeedForwardNetwork.create(genome, config)
-        for xi, xo in zip(xor_inputs, xor_outputs):
-            output = net.activate(xi)
-            genome.fitness -= (output[0] - xo[0]) ** 2
+def to_torch_dataset_1d(opt, t, yt):
+    t = torch.from_numpy(t).view(-1, 1).float()
+    yt = torch.from_numpy(yt).view(-1, 1).float()
+    # if opt.CUDA:
+    #     t = t.cuda()
+    #     yt = yt.cuda()
+    return t, yt
 
+# the dataset should 1. yield the model 2. narrow the search space
 
-def run(config_file):
-    # Load configuration.
-    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                         config_file)
+# searching for models
+# dissect https://github.com/ianwhale/nsga-net for bitstring encoding, model deduplication
+# look at https://github.com/nightstorm0909/NEvoNAS for how to structure actual search space / cells + implemenation of novelty search
 
-    # Create the population, which is the top-level object for a NEAT run.
-    p = neat.Population(config)
-
-    print(p)
-
-    # Add a stdout reporter to show progress in the terminal.
-    p.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    p.add_reporter(stats)
-
-    #creates a checkpoint at the 10th generation!
-    p.add_reporter(neat.Checkpointer(11)) 
-
-   
-    # this starts at generation 0
-    # run evolution algorithm run with the eval_genomes fitness function - for 10 generations
-    winner = p.run(eval_genomes, 10)
-    # winner returns the best genome seen
-
-
-
-    # Display the winning genome.
-    print('\nBest genome:\n{!s}'.format(winner))
-
-    # Show output of the most fit genome against training data.
-    print('\nOutput:')
-    winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
-    for xi, xo in zip(xor_inputs, xor_outputs):
-        output = winner_net.activate(xi)
-        print("input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
-
-    node_names = {-1: 'A', -2: 'B', 0: 'A XOR B'}
-  
-
-    p.run(eval_genomes, 10) # run evolution algorithm run with the eval_genomes fitness function - for 10 generations
-
-if __name__ == '__main__':
-    # Determine path to configuration file. This path manipulation is
-    # here so that the script will run successfully regardless of the
-    # current working directory.
-    local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, 'config-feedforward')
-    run(config_path)
+# honestly, CRDTs could help with encoding, deduplication, and novelty search. seemed like too much work before but it may not be
