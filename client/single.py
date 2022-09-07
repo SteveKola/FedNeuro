4326ac07bc114a025ce973ba97267ada0fd3422d
✨ <e@universe.local>
2022-09-07 19:32:35 -0400

    nni

--- client/single.py
+++ client/single.py
@@ -1,17 +1,23 @@
-from nni.nas.pytorch import mutables
+from os import environ
 from LSUV import LSUVinit
 
+import torch
+import torch.nn.functional as F
+import nni.retiarii.nn.pytorch as nn
+from nni.retiarii import model_wrapper
 
-class Net(nn.Module):
+
+@model_wrapper 
+class ModelSpace(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = mutables.LayerChoice([
+        super(ModelSpace, self).__init__()
+        self.conv1 = nn.LayerChoice([
             nn.Conv2d(1, 32, 3, 1),
             nn.Conv2d(1, 32, 5, 3)
         ])  # try 3x3 kernel and 5x5 kernel
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.conv3 = nn.Conv2d(64, 64, 1, 1)
-        self.skipcon = mutables.InputChoice(n_candidates=1)
+        self.skipcon = nn.InputChoice(n_candidates=1)
         self.dropout1 = nn.Dropout2d(0.25)
         self.dropout2 = nn.Dropout2d(0.5)
         self.fc1 = nn.Linear(9216, 128)
@@ -29,7 +35,6 @@ class Net(nn.Module):
         x = F.max_pool2d(x, 2)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
-
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
@@ -39,39 +44,47 @@ class Net(nn.Module):
         output = F.log_softmax(x, dim=1)
         return output
 
-phenotype = Net()
-model = LSUVinit(phenotype,data)
+
+
+model_space = ModelSpace()
+
+# I need to give the model MNIST data to init weights. This will make search take less time.
+# I also need to
+# print(model_space)
+# to see if everything works up to this point, but I hate working with Python locally. 
+# I think I need to move to some cloud environment because installing packages locally sucks.
+model = LSUVinit(model_space,data)
 
 
 
 
-dataset_train = CIFAR10(root="./data", train=True, download=True, transform=train_transform)
-dataset_valid = CIFAR10(root="./data", train=False, download=True, transform=valid_transform)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), 0.05, momentum=0.9, weight_decay=1.0E-4)
+# dataset_train = MNIST(root="./data", train=True, download=True, transform=train_transform)
+# dataset_valid = MNIST(root="./data", train=False, download=True, transform=valid_transform)
+# criterion = nn.CrossEntropyLoss()
+# optimizer = torch.optim.SGD(model.parameters(), 0.05, momentum=0.9, weight_decay=1.0E-4)
 
 
-# use NAS here
-def top1_accuracy(output, target):
-    # this is the function that computes the reward, as required by ENAS algorithm
-    batch_size = target.size(0)
-    _, predicted = torch.max(output.data, 1)
-    return (predicted == target).sum().item() / batch_size
+# # use NAS here
+# def top1_accuracy(output, target):
+#     # this is the function that computes the reward, as required by ENAS algorithm
+#     batch_size = target.size(0)
+#     _, predicted = torch.max(output.data, 1)
+#     return (predicted == target).sum().item() / batch_size
 
-def metrics_fn(output, target):
-    # metrics function receives output and target and computes a dict of metrics
-    return {"acc1": top1_accuracy(output, target)}
+# def metrics_fn(output, target):
+#     # metrics function receives output and target and computes a dict of metrics
+#     return {"acc1": top1_accuracy(output, target)}
 
-from nni.algorithms.nas.pytorch import enas
-trainer = enas.EnasTrainer(model,
-                           loss=criterion,
-                           metrics=metrics_fn,
-                           reward_function=top1_accuracy,
-                           optimizer=optimizer,
-                           batch_size=128
-                           num_epochs=10,  # 10 epochs
-                           dataset_train=dataset_train,
-                           dataset_valid=dataset_valid,
-                           log_frequency=10)  # print log every 10 steps
-trainer.train()  # training
-trainer.export(file="model_dir/final_architecture.json")  # export the final architecture to file
+# from nni.algorithms.nas.pytorch import enas
+# trainer = enas.EnasTrainer(model,
+#                            loss=criterion,
+#                            metrics=metrics_fn,
+#                            reward_function=top1_accuracy,
+#                            optimizer=optimizer,
+#                            batch_size=128
+#                            num_epochs=10,  # 10 epochs
+#                            dataset_train=dataset_train,
+#                            dataset_valid=dataset_valid,
+#                            log_frequency=10)  # print log every 10 steps
+# trainer.train()  # training
+# trainer.export(file="model_dir/final_architecture.json")  # export the final architecture to file
