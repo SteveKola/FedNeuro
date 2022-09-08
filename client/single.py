8387eed76bc7637eead5f39c76335678ef10388a
stevekola <kolawolesteven99@gmail.com>
2022-09-08 16:56:04 +0100

    retrieve Torch's built-in MNIST data for testing

--- client/single.py
+++ client/single.py
@@ -9,14 +9,18 @@ import nni.retiarii.nn.pytorch as nn
 from nni.retiarii import model_wrapper
 import nni.retiarii.strategy as strategy
 
-@model_wrapper 
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+
+@model_wrapper
 class ModelSpace(nn.Module):
     def __init__(self):
         super(ModelSpace, self).__init__()
-        self.conv1 = nn.LayerChoice([
-            nn.Conv2d(1, 32, 3, 1),
-            nn.Conv2d(1, 32, 5, 3)
-        ])  # try 3x3 kernel and 5x5 kernel
+        self.conv1 = nn.LayerChoice(
+            [nn.Conv2d(1, 32, 3, 1), nn.Conv2d(1, 32, 5, 3)]
+        )  # try 3x3 kernel and 5x5 kernel
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.conv3 = nn.Conv2d(64, 64, 1, 1)
         self.skipcon = nn.InputChoice(n_candidates=1)
@@ -24,7 +28,6 @@ class ModelSpace(nn.Module):
         self.dropout2 = nn.Dropout2d(0.5)
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
-
 
     def forward(self, x):
         x = self.conv1(x)
@@ -47,25 +50,44 @@ class ModelSpace(nn.Module):
         return output
 
 
-
 model_space = ModelSpace()
 
 # we need to give the model MNIST data to initialize weights. This will make search take less time.
+training_data = datasets.MNIST(
+    root="data", train=True, download=True, transform=ToTensor()
+)
 
-model = LSUVinit(model_space,data)
+test_data = datasets.MNIST(
+    root="data", train=False, download=True, transform=ToTensor()
+)
+model = LSUVinit(model_space, training_data)
 
-search_strategy = strategy.RegularizedEvolution(optimize_mode='maximize',population_size='300',cycles='100',on_failure='worst')
+search_strategy = strategy.RegularizedEvolution(
+    optimize_mode="maximize", population_size="300", cycles="100", on_failure="worst"
+)
 
 import nni.retiarii.evaluator.pytorch.lightning as pl
 from torchvision import transforms
 
-transform = nni.trace(transforms.Compose, [nni.trace(transforms.ToTensor()), nni.trace(transforms.Normalize, (0.1307,), (0.3081,))])
-train_dataset = nni.trace(model, root='data/mnist', train=True, download=True, transform=transform)
-test_dataset = nni.trace(model, root='data/mnist', train=False, download=True, transform=transform)
+transform = nni.trace(
+    transforms.Compose,
+    [
+        nni.trace(transforms.ToTensor()),
+        nni.trace(transforms.Normalize, (0.1307,), (0.3081,)),
+    ],
+)
+train_dataset = nni.trace(
+    model, root="data/mnist", train=True, download=True, transform=transform
+)
+test_dataset = nni.trace(
+    model, root="data/mnist", train=False, download=True, transform=transform
+)
 
 # pl.DataLoader and pl.Classification is already traced and supports serialization.
-evaluator = pl.Classification(train_dataloaders=pl.DataLoader(train_dataset, batch_size=100),
-                              val_dataloaders=pl.DataLoader(test_dataset, batch_size=100),
-                              max_epochs=10)
+evaluator = pl.Classification(
+    train_dataloaders=pl.DataLoader(train_dataset, batch_size=100),
+    val_dataloaders=pl.DataLoader(test_dataset, batch_size=100),
+    max_epochs=10,
+)
 
 # using https://nni.readthedocs.io/en/stable/nas/evaluator.html
